7c9f7842bdb9b8a843ffb051dfa6173baf6671b7
Angelina Myagkova <aa.myagkova@gmail.com>
2021-07-04 22:42:38 +0200

    update nginx

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,11 +3,11 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = ${SECRET_KEY}
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('NGINX_HOST')]
+ALLOWED_HOSTS = [${NGINX_PORT}]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
