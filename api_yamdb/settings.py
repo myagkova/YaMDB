80b376da931461f05eea24db05825b08bafeb5df
Angelina Myagkova <aa.myagkova@gmail.com>
2021-07-05 17:11:42 +0200

    nginx

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "other")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['130.193.52.118']
+ALLOWED_HOSTS = ['130.193.52.118', ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
