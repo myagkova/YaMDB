836c497f0836d7c6cb0fe0d5b8559ab09163c907
Angelina Myagkova <aa.myagkova@gmail.com>
2021-07-05 16:23:34 +0200

    update nginx

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'dummy key for unit tests')
 
 DEBUG = False
 
